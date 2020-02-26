b44953e981da13ca9b0b71870a5abe35678642de
davidminaya <dminaya@intellisys.com.do>
2020-02-26 10:32:07 -0400

    Add style to the error page

--- views/error.view.php
+++ views/error.view.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="styles/error.css">
 </head>
 <body>
-  <h1>Error 404</h1>
+  <div class="error">Error 404</div>
+  <div class="message">Page not found</div>
 </body>
 </html>
